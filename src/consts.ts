7a4cc5b299b4675599e5d0621b29b835f9bde147
Amal221200 <amalmurikkoli2000@gmail.com>
2026-01-25 12:41:53 +0530

    feat: home page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Side Canon';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Explore amazing fan fiction stories from your favorite franchises, written by SideCannon.';
