29416afe80729f92e82c937c8ca3bcb928af1129
Amal221200 <amalmurikkoli2000@gmail.com>
2026-01-25 18:02:40 +0530

    fix: updated domain in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://side-canon.vercel.app",
   integrations: [mdx(), sitemap()],
 
   vite: {
