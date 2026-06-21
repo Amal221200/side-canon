9da533ff7510568fc9683ade75ed8993c16896fe
Amal221200 <amalmurikkoli2000@gmail.com>
2026-06-21 13:34:27 +0530

    fix: returning onRequest from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,11 @@
 import { defineMiddleware } from "astro:middleware";
 
-
+export const onRequest = defineMiddleware((_, next) => {
+  // return new Response("Temporarily unavailable", {
+  //   status: 503,
+  //   headers: {
+  //     "Content-Type": "text/plain",
+  //   },
+  // });
+  return next();
+});
